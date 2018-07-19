f06bdf42581ed654db730d4921d67cda7112dc71
Karl <pvial@krypt.local>
2018-07-18 18:41:51 -0700

    FastPortScan v0.2 TCP scanning capability complete

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # FastPortScan
 Threaded TCP port scanner
+6 minutes 28 seconds to scan all localhost TCP ports with default settings
 
 # Usage:
 fps = FastPortScan()
-fps.scan("host", 65536, 32)
+fps.scan("localhost", [80,443])
+
+or 
+
+fps.scan("localhost", range(65536))
+
+or slow sequential scan which returns a list of ports
+
+openports = fps.slowscan("localhost", range(65536))
--- __init__.py
+++ __init__.py
@@ -4,24 +4,36 @@ import time
 from multiprocessing import Process
 
 class FastPortScan:
-    def __init__(self, mode=None):
+    def __init__(self, mode=None, timeout=0.5, threads=32):
         self.mode = mode
+        self.timeout = timeout
+        if threads < 1:
+            self.threads = 1
+        else:
+            self.threads = threads
 
-    def scan(self, host, ports=65536, threads=32):
-        self.dividepools(host, ports, threads)
+    def scan(self, host, ports=[]):
+        self.dividepools(host, ports, self.threads)
 
     def dividepools(self, host, ports, threads):
-        a = 1
-        chunk = ports / threads
-        z = chunk * 2
-        for thread in range(1,threads):
-            Process(target=self.scanpool, args=(host,a,z)).start()
-            a = z + 1
-            z = z + chunk
+        start = 1
+        if len(ports) < threads:
+            threads = len(ports)
+        chunks = len(ports) / threads
+        extra = len(ports) % threads
+        if extra > 0:
+            chunks += 1
+        for thread in range(threads):
+            selected_ports = []
+            if len(ports) < chunks:
+                chunks = len(ports)
+            for c in range(chunks):
+                selected_ports.append(ports.pop(0))
+            Process(target=self.scanpool, args=(host,selected_ports)).start()
 
-    def scanpool(self, host, a, z):
+    def scanpool(self, host, ports):
         openports = []
-        for port in range(a,z):
+        for port in ports:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.settimeout(2)
             try:
@@ -30,10 +42,23 @@ class FastPortScan:
                sys.stdout.write("Detected open TCP port: %i" % port)
                sys.stdout.write("\n")
                openports.append(port)
+               s.close()
+            except socket.error as er:
+               #sys.stdout.write("Detected closed TCP port: %i" % port)
+               #sys.stdout.write("\n")
+               continue
+    
+    def slowscan(self, host, ports):
+        openports = []
+        for port in ports:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(2)
+            try:
+               s.settimeout(self.timeout)
+               s.connect((host, port))
+               #sys.stdout.write("Detected open TCP port: %i" % port)
+               #sys.stdout.write("\n")
+               openports.append(port)
             except socket.error as er:
                s.close()
-               s = None
-               continue
-
-fps = FastPortScan()
-fps.scan("localhost", 65536, 32)
+        return openports
